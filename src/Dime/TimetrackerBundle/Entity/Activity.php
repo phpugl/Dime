3daf06b80d470d9c3a9b2146f230b995e3b3a559
Danilo Kuehn <dk@nogo-software.de>
2011-11-12 16:12:10 +0100

    add Activitiy entity
    app/console generate:doctrine:entities DimeTimetrackerBundle:Activity

---
+++ src/Dime/TimetrackerBundle/Entity/Activity.php
@@ -0,0 +1,267 @@
+<?php
+namespace Dime\TimetrackerBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use Doctrine\Common\Collections\ArrayCollection;
+
+/**
+ * Dime\TimetrackerBundle\Entity\Activity
+ *
+ * @ORM\Table(name="activities")
+ * @ORM\Entity(repositoryClass="Dime\TimetrackerBundle\Entity\ActivityRepository")
+ */
+class Activity {
+
+    /**
+     * @var integer $id
+     * 
+     * @ORM\Id
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    protected $id;
+    
+    /**
+     * @var integer $user
+     *
+     * @ORM\ManyToOne(targetEntity="User", inversedBy="activities")
+     * @ORM\JoinColumn(name="user_id", referencedColumnName="id", nullable=false)
+     */
+    protected $user;
+    
+    /**
+     * @var integer $duration
+     *
+     * @ORM\Column(type="int", length=255, nullable=true)
+     */
+    protected $duration;
+    
+    /**
+     * @var Date $startedAt
+     *
+     * @ORM\Column(name="started_at", type="datetime", nullable=true)
+     */
+    protected $startedAt;
+
+    /**
+     * @var Date $stoppedAt
+     *
+     * @ORM\Column(name="stopped_at", type="datetime", nullable=true)
+     */
+    protected $stoppedAt;
+    
+    /**
+     * @var string $description
+     *
+     * @ORM\Column(type="text", nullable=true)
+     */
+    protected $description;
+    
+    /**
+     * @var float $rate
+     *
+     * @ORM\Column(type="decimal", nullable=true)
+     */
+    protected $rate;
+    
+    /**
+     * @var string $rateReference
+     *
+     * @ORM\Column(name="rate_reference", type="string", length=255, nullable=true)
+     */
+    protected $rateReference;
+    
+    /**
+     * @var string $status
+     *
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    protected $status;
+    
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set duration
+     *
+     * @param integer $duration
+     * @return Activity
+     */
+    public function setDuration($duration)
+    {
+        $this->duration = $duration;
+        return $this;
+    }
+
+    /**
+     * Get duration
+     *
+     * @return int 
+     */
+    public function getDuration()
+    {
+        return $this->duration;
+    }
+
+    /**
+     * Set startedAt
+     *
+     * @param datetime $startedAt
+     * @return Activity
+     */
+    public function setStartedAt($startedAt)
+    {
+        $this->startedAt = $startedAt;
+        return $this;
+    }
+
+    /**
+     * Get startedAt
+     *
+     * @return datetime 
+     */
+    public function getStartedAt()
+    {
+        return $this->startedAt;
+    }
+
+    /**
+     * Set stoppedAt
+     *
+     * @param datetime $stoppedAt
+     * @return Activity
+     */
+    public function setStoppedAt($stoppedAt)
+    {
+        $this->stoppedAt = $stoppedAt;
+        return $this;
+    }
+
+    /**
+     * Get stoppedAt
+     *
+     * @return datetime 
+     */
+    public function getStoppedAt()
+    {
+        return $this->stoppedAt;
+    }
+
+    /**
+     * Set description
+     *
+     * @param text $description
+     * @return Activity
+     */
+    public function setDescription($description)
+    {
+        $this->description = $description;
+        return $this;
+    }
+
+    /**
+     * Get description
+     *
+     * @return text 
+     */
+    public function getDescription()
+    {
+        return $this->description;
+    }
+
+    /**
+     * Set rate
+     *
+     * @param decimal $rate
+     * @return Activity
+     */
+    public function setRate($rate)
+    {
+        $this->rate = $rate;
+        return $this;
+    }
+
+    /**
+     * Get rate
+     *
+     * @return decimal 
+     */
+    public function getRate()
+    {
+        return $this->rate;
+    }
+
+    /**
+     * Set rateReference
+     *
+     * @param string $rateReference
+     * @return Activity
+     */
+    public function setRateReference($rateReference)
+    {
+        $this->rateReference = $rateReference;
+        return $this;
+    }
+
+    /**
+     * Get rateReference
+     *
+     * @return string 
+     */
+    public function getRateReference()
+    {
+        return $this->rateReference;
+    }
+
+    /**
+     * Set status
+     *
+     * @param string $status
+     * @return Activity
+     */
+    public function setStatus($status)
+    {
+        $this->status = $status;
+        return $this;
+    }
+
+    /**
+     * Get status
+     *
+     * @return string 
+     */
+    public function getStatus()
+    {
+        return $this->status;
+    }
+
+    /**
+     * Set user
+     *
+     * @param Dime\TimetrackerBundle\Entity\User $user
+     * @return Activity
+     */
+    public function setUser(\Dime\TimetrackerBundle\Entity\User $user)
+    {
+        $this->user = $user;
+        return $this;
+    }
+
+    /**
+     * Get user
+     *
+     * @return Dime\TimetrackerBundle\Entity\User 
+     */
+    public function getUser()
+    {
+        return $this->user;
+    }
+}
