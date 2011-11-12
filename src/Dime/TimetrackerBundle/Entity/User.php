6c3d58fd0fda0250c01a7c52fe0a97e48c7cb990
timheil <tim.heil@webentwickler.cc>
2011-11-12 18:26:41 +0100

    Model: adds user enitity

---
+++ src/Dime/TimetrackerBundle/Entity/User.php
@@ -0,0 +1,122 @@
+<?php
+namespace Dime\TimetrackerBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use Doctrine\Common\Collections\ArrayCollection;
+
+/**
+ * Dime\TimetrackerBundle\Entity\Project
+ *
+ * @ORM\Table(name="users")
+ * @ORM\Entity(repositoryClass="Dime\TimetrackerBundle\Entity\UserRepository")
+ */
+class User {
+
+    /**
+     * @var integer $id
+     *
+     * @ORM\Id
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    protected $id;
+
+    /**
+     * @var string $duration
+     *
+     * @ORM\Column(type="string", length=255)
+     */
+    protected $firstname;
+
+    /**
+     * @var string $duration
+     *
+     * @ORM\Column(type="string", length=255)
+     */
+    protected $lastname;
+
+    /**
+     * email
+     *
+     * @var string
+     * @ORM\Column(type="email", length=255)
+     */
+    protected $email;
+
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set firstname
+     *
+     * @param string $firstname
+     * @return User
+     */
+    public function setFirstname($firstname)
+    {
+        $this->firstname = $firstname;
+        return $this;
+    }
+
+    /**
+     * Get firstname
+     *
+     * @return string 
+     */
+    public function getFirstname()
+    {
+        return $this->firstname;
+    }
+
+    /**
+     * Set lastname
+     *
+     * @param string $lastname
+     * @return User
+     */
+    public function setLastname($lastname)
+    {
+        $this->lastname = $lastname;
+        return $this;
+    }
+
+    /**
+     * Get lastname
+     *
+     * @return string 
+     */
+    public function getLastname()
+    {
+        return $this->lastname;
+    }
+
+    /**
+     * Set email
+     *
+     * @param email $email
+     * @return User
+     */
+    public function setEmail(\email $email)
+    {
+        $this->email = $email;
+        return $this;
+    }
+
+    /**
+     * Get email
+     *
+     * @return email 
+     */
+    public function getEmail()
+    {
+        return $this->email;
+    }
+}
