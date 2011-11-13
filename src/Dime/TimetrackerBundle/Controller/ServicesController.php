103380fa0ed770b189c04a0f4ec32aa7c79f6878
Thomas Birke <quafzi@netextreme.de>
2011-11-13 11:39:50 +0100

    first dummy json action

--- src/Dime/TimetrackerBundle/Controller/ServicesController.php
+++ src/Dime/TimetrackerBundle/Controller/ServicesController.php
@@ -4,6 +4,7 @@ namespace Dime\TimetrackerBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use FOS\RestBundle\View\View;
 
 class ServicesController extends Controller
 {
@@ -12,6 +13,8 @@ class ServicesController extends Controller
      */
     public function indexAction()
     {
-        return array();
+        $view = View::create()->setStatusCode(200);
+        $view->setData(/* data to send */);
+        return $this->get('fos_rest.view_handler')->handle($view);
     }
 }
