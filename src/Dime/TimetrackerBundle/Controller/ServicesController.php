51310b02a511cdd1c94cc94f64d5073624634455
Thomas Birke <quafzi@netextreme.de>
2011-11-13 16:48:13 +0100

    implemented service api actions

--- src/Dime/TimetrackerBundle/Controller/ServicesController.php
+++ src/Dime/TimetrackerBundle/Controller/ServicesController.php
@@ -32,7 +32,19 @@ class ServicesController extends Controller
      * @param int $id
      */
     public function getServiceAction($id)
-    {}
+    {
+        $service = $this->getDoctrine()->getRepository('Dime\TimetrackerBundle\Entity\Service')->find($id);
+        if ($service) {
+            $data = array();
+            $data['id']          = $service->getId();
+            $data['name']        = $service->getName();
+            $data['description'] = $service->getDescription();
+            $data['rate']        = $service->getRate();
+        }
+        $view = View::create()->setStatusCode(200);
+        $view->setData($data);
+        return $this->get('fos_rest.view_handler')->handle($view);
+    }
 
     /**
      * create service
@@ -41,7 +53,11 @@ class ServicesController extends Controller
      * @return void
      */
     public function postServicesAction()
-    {}
+    {
+        $this->persist(new Service());
+
+        return $view;
+    }
 
     /**
      * modify service
@@ -51,7 +67,31 @@ class ServicesController extends Controller
      * @return void
      */
     public function putServicesAction($id)
-    {}
+    {
+        $service = $this->getDoctrine()->getRepository('Dime\TimetrackerBundle\Entity\Service')->find($id);
+        if ($service) {
+            $this->persist($service);
+        }
+        return $view;
+    }
+
+    /**
+     * persist service
+     * 
+     * @param Dime\TimetrackerBundle\Entity\Service $service 
+     */
+    protected function persist(Dime\TimetrackerBundle\Entity\Service $service)
+    {
+        $form = $this->getForm($service);
+
+        $form->bindRequest($request);
+
+        if ($form->isValid()) {
+            $em = $this->getDoctrine()->getEntityManager();
+            $em->persist($service);
+            $em->flush();
+        }
+    }
 
     /**
      * delete service
@@ -61,5 +101,19 @@ class ServicesController extends Controller
      * @return void
      */
     public function deleteServicesAction($id)
-    {}
+    {
+        if ($service = $this->getDoctrine()->getRepository('Dime\TimetrackerBundle\Entity\Service')->find($id)) {
+            $em = $this->getDoctrine()->getEntityManager();
+            $em->remove($service);
+        }
+    }
+
+    protected function getForm($service)
+    {
+        return $this->formFactory->createBuilder('form', $service)
+            ->add('name',        'string',  array('required' => true))
+            ->add('description', 'text',    array('required' => false))
+            ->add('rate',        'decimal', array('required' => false))
+            ->getForm();
+    }
 }
