8495145f4a5c9bb8b57cb24863ee747adf4389e1
Thomas Birke <quafzi@netextreme.de>
2011-11-13 13:26:24 +0100

    add delete action for services and replace slug by id

--- src/Dime/TimetrackerBundle/Controller/ServicesController.php
+++ src/Dime/TimetrackerBundle/Controller/ServicesController.php
@@ -16,34 +16,45 @@ class ServicesController extends Controller
     public function getServicesAction()
     {
         $view = View::create()->setStatusCode(200);
-        $view->setData(/* data to send */);
+        $view->setData(array('foo' => 'bar'));
         return $this->get('fos_rest.view_handler')->handle($view);
     }
 
     /**
      * load service
-     *
      * [GET] /services/{slug}
+     *
+     * @param int $id
      */
-    public function getServiceAction($slug)
+    public function getServiceAction($id)
     {}
 
     /**
      * create service
      * [POST] /services
-     * 
+     *
      * @return void
      */
-    public function postServiceAction()
+    public function postServicesAction()
     {}
 
     /**
      * modify service
-     * [PUT] /services/{slug}
-     * 
-     * @param string $slug 
+     * [PUT] /services/{id}
+     *
+     * @param int $id
      * @return void
      */
-    public function putServiceAction($slug)
+    public function putServicesAction($id)
+    {}
+
+    /**
+     * delete service
+     * [DELETE] /services/{id}
+     *
+     * @param int $id
+     * @return void
+     */
+    public function deleteServicesAction($id)
     {}
 }
