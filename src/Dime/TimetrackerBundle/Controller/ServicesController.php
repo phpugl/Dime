3028172611c7a09fff4eb63190ffb7dc7bb8e2dd
Thomas Birke <quafzi@netextreme.de>
2011-11-13 12:41:34 +0100

    add service rest controller actions

--- src/Dime/TimetrackerBundle/Controller/ServicesController.php
+++ src/Dime/TimetrackerBundle/Controller/ServicesController.php
@@ -9,12 +9,41 @@ use FOS\RestBundle\View\View;
 class ServicesController extends Controller
 {
     /**
+     * [GET] /services
+     *
      * @Route("/")
      */
-    public function indexAction()
+    public function getServicesAction()
     {
         $view = View::create()->setStatusCode(200);
         $view->setData(/* data to send */);
         return $this->get('fos_rest.view_handler')->handle($view);
     }
+
+    /**
+     * load service
+     *
+     * [GET] /services/{slug}
+     */
+    public function getServiceAction($slug)
+    {}
+
+    /**
+     * create service
+     * [POST] /services
+     * 
+     * @return void
+     */
+    public function postServiceAction()
+    {}
+
+    /**
+     * modify service
+     * [PUT] /services/{slug}
+     * 
+     * @param string $slug 
+     * @return void
+     */
+    public function putServiceAction($slug)
+    {}
 }
