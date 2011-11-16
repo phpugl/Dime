0fc3ac53fe7f2193e97f2cd6b6e00db82c249508
Thomas Birke <quafzi@netextreme.de>
2011-11-16 22:28:00 +0100

    add methods for application api

--- src/Dime/TimetrackerBundle/Controller/ActivitiesController.php
+++ src/Dime/TimetrackerBundle/Controller/ActivitiesController.php
@@ -24,4 +24,96 @@ class ActivitiesController extends Controller
 
         return $this->get('fos_rest.view_handler')->handle($view);
     }
+
+    /**
+     * load activity
+     *
+     * [GET] /activity/{id}
+     */
+    public function getActivityAction($id)
+    {
+        $activity = $this->getDoctrine()->getRepository('DimeTimetrackerBundle:Activity')->find($id);
+        if ($activity) {
+            $view = View::create()->setStatusCode(200);
+            $view->setData(array(
+                'id'    => $activity->getId(),
+                'name'  => $activity->getName(),
+                'alias' => $activity->getAlias()
+            ));
+        } else {
+            $view = View::create()->setStatusCode(404);
+        }
+        return $this->get('fos_rest.view_handler')->handle($view);
+    }
+
+    /**
+     * create activity
+     * [POST] /activity
+     * 
+     * @return void
+     */
+    public function postActivityAction()
+    {
+        $activity = new Activity();
+        $form = $this->getForm($activity);
+        $form->bindRequest($this->getRequest());
+        $this->persist($form, $activity);
+        $view->setData($form->getData());
+        return $this->get('fos_rest.view_handler')->handle($view);
+    }
+
+    /**
+     * modify activity
+     * [PUT] /activity/{id}
+     * 
+     * @param string $id
+     * @return void
+     */
+    public function putActivityAction($id)
+    {
+        $activity = $this->getDoctrine()->getRepository('DimeTimetrackerBundle:Activity')->find($id);
+        $form = $this->getForm($activity);
+        $this->persist($form, $activity);
+        $view->setData($form->getData());
+        return $this->get('fos_rest.view_handler')->handle($view);
+    }
+
+    /**
+     * delete activity
+     * [DELETE] /activity/{id}
+     *
+     * @param int $id
+     * @return void
+     */
+    public function deleteActivityAction($id)
+    {
+        if ($activity = $this->getDoctrine()->getRepository('DimeTimetrackerBundle:Activity')->find($id)) {
+            $em = $this->getDoctrine()->getEntityManager();
+            $em->remove($activity);
+        }
+    }
+
+    /**
+     * persist activity
+     * 
+     * @param $form
+     * @param Dime\TimetrackerBundle\Entity\Activity $activity
+     */
+    protected function persist($form, Dime\TimetrackerBundle\Entity\Activity $activity)
+    {
+        $form->bindRequest($this->getRequest());
+        if ($form->isValid()) {
+            $em = $this->getDoctrine()->getEntityManager();
+            $em->persist($activity);
+            $em->flush();
+        }
+    }
+
+    protected function getForm($activity)
+    {
+        return $this->formFactory->createBuilder('form', $activity)
+            ->add('name',  'string', array('required' => true))
+            ->add('alias', 'string', array('required' => true))
+            ->getForm();
+    }
 }
