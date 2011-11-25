f3b9514b27ba6ccd894efbde4271d10e4f1a8d17
Danilo Kühn <dk@nogo-software.de>
2011-11-25 11:31:43 +0100

    merge conflict: service->toJson

--- src/Dime/TimetrackerBundle/Controller/ServicesController.php
+++ src/Dime/TimetrackerBundle/Controller/ServicesController.php
@@ -5,6 +5,9 @@ namespace Dime\TimetrackerBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use FOS\RestBundle\View\View;
+use Dime\TimetrackerBundle\Entity\Service;
+use Dime\TimetrackerBundle\Form\ServiceType;
+
 
 class ServicesController extends Controller
 {
@@ -15,10 +18,12 @@ class ServicesController extends Controller
      */
     public function getServicesAction()
     {
-        $services = $this->getDoctrine()->getRepository('Dime\TimetrackerBundle\Entity\Service')->findAll();
+        $em = $this->getDoctrine()->getEntityManager();
+        
+        $services = $em->getRepository('DimeTimetrackerBundle:Service')->findAll();
         $data = array();
         foreach ($services as $service) {
-            $data[$service->getId()] = $service->getName();
+            $data[] = $service->toJson();
         }
         $view = View::create()->setStatusCode(200);
         $view->setData($data);
@@ -28,23 +33,20 @@ class ServicesController extends Controller
 
     /**
      * load service
-     * [GET] /services/{slug}
+     * [GET] /services/{id}
      *
      * @param int $id
      */
     public function getServiceAction($id)
     {
-        $service = $this->getDoctrine()->getRepository('Dime\TimetrackerBundle\Entity\Service')->find($id);
+        $em = $this->getDoctrine()->getEntityManager();
+        $service = $em->getRepository('DimeTimetrackerBundle:Service')->find($id);
         if ($service) {
             $view = View::create()->setStatusCode(200);
-            $data = array();
-            $data['id']          = $service->getId();
-            $data['name']        = $service->getName();
-            $data['description'] = $service->getDescription();
-            $data['rate']        = $service->getRate();
-            $view->setData($data);
+            $view->setData($service->toJson());
         } else {
             $view = View::create()->setStatusCode(404);
+            $view->setData("Service does not exists.");
         }
         return $this->get('fos_rest.view_handler')->handle($view);
     }
@@ -58,10 +60,33 @@ class ServicesController extends Controller
     public function postServicesAction()
     {
         $service = new Service();
-        $form = $this->getForm($service);
-        $form->bindRequest($this->getRequest());
-        $this->persist($form);
-        $view->setData($form->getData());
+        
+        // create service form
+        $form = $this->createForm(new ServiceType(), $service);
+        
+        
+        $request = $this->getRequest();
+    
+        // convert json to assoc array
+        $data = json_decode($request->getContent(), true);
+        
+        // bind data to form
+        $form->bind($data);
+
+        if ($form->isValid()) {
+            // save change to database
+            $em->persist($service);
+            $em->flush();
+
+            // push back the new object
+            $view = View::create()->setStatusCode(200);
+            $view->setData($service->toJson());
+        } else {
+            // return error sting from form
+            $view = View::create()->setStatusCode(404);
+            $view->setData($form->getErrorsAsString());
+        }
+        
         return $this->get('fos_rest.view_handler')->handle($view);
     }
 
@@ -74,14 +99,43 @@ class ServicesController extends Controller
      */
     public function putServicesAction($id)
     {
-        $service = $this->getDoctrine()->getRepository('Dime\TimetrackerBundle\Entity\Service')->find($id);
+        $em = $this->getDoctrine()->getEntityManager();
+        $service = $em->getRepository('DimeTimetrackerBundle:Service')->find($id);
+                
         if ($service) {
-            $view = View::create()->setStatusCode(200);
-            $form = $this->getForm($service);
-            $this->persist($form, $service);
-            $view->setData($form->getData());
+            $request = $this->getRequest();
+            
+            // convert json to assoc array
+            $data = json_decode($request->getContent(), true);
+            
+            // clean id from array
+            if (isset($data['id'])) {
+                unset($data['id']);
+            }
+            
+            // create service form
+            $form = $this->createForm(new ServiceType(), $service);
+            
+            // bind data to form
+            $form->bind($data);
+            
+            // check if is valid
+            if ($form->isValid()) {
+                // save change to database
+                $em->persist($service);
+                $em->flush();
+                
+                // push back the new object
+                $view = View::create()->setStatusCode(200);
+                $view->setData($service->toJson());
+            } else {
+                // return error sting from form
+                $view = View::create()->setStatusCode(404);
+                $view->setData($form->getErrorsAsString());
+            }
         } else {
             $view = View::create()->setStatusCode(404);
+            $view->setData("Service does not exists.");
         }
         return $this->get('fos_rest.view_handler')->handle($view);
     }
@@ -95,34 +149,18 @@ class ServicesController extends Controller
      */
     public function deleteServicesAction($id)
     {
-        if ($service = $this->getDoctrine()->getRepository('Dime\TimetrackerBundle\Entity\Service')->find($id)) {
-            $em = $this->getDoctrine()->getEntityManager();
+        $em = $this->getDoctrine()->getEntityManager();
+        $service = $em->getRepository('DimeTimetrackerBundle:Service')->find($id);
+        
+        if ($service) {
             $em->remove($service);
-        }
-    }
-
-    /**
-     * persist service
-     * 
-     * @param $form
-     * @param Dime\TimetrackerBundle\Entity\Service $service
-     */
-    protected function persist($form, Dime\TimetrackerBundle\Entity\Service $service)
-    {
-        $form->bindRequest($this->getRequest());
-        if ($form->isValid()) {
-            $em = $this->getDoctrine()->getEntityManager();
-            $em->persist($service);
             $em->flush();
+            
+            $view = View::create()->setStatusCode(200);
+            $view->setData("Service has been removed.");
+        } else {
+            $view = View::create()->setStatusCode(404);
+            $view->setData("Service does not exists.");
         }
-    }
-
-    protected function getForm($service)
-    {
-        return $this->formFactory->createBuilder('form', $service)
-            ->add('name',        'string',  array('required' => true))
-            ->add('description', 'text',    array('required' => false))
-            ->add('rate',        'decimal', array('required' => false))
-            ->getForm();
     }
 }
