076eaca0b1f0578b9d1649befd31ca29764bc857
Danilo Kühn <dk@nogo-software.de>
2011-12-14 18:37:02 +0100

    add DimeTestCase

---
+++ src/Dime/TimetrackerBundle/Tests/Controller/DimeTestCase.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Dime\TimetrackerBundle\Tests\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class DimeTestCase extends WebTestCase
+{
+    public function setUp()
+    {
+        $this->client = static::createClient();
+    }
+
+    /**
+     * do a request using HTTP authentification 
+     */
+    protected function request(
+        $method,
+        $url,
+        $parameters=array(), 
+        $files=array(),
+        $server=null,
+        $content=null
+    ) {
+        if (is_null($server)) {
+            $server=array('PHP_AUTH_USER' => 'admin', 'PHP_AUTH_PW' => 'kitten');
+        }
+        $this->client->restart();
+
+        // make get request with authentifaction 
+        $this->client->request($method, $url, $parameters, $files, $server, $content);
+        return $this->client->getResponse();
+    }
+}
