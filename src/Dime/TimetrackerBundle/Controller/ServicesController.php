9555e398c68583d52a99e7bfd2f808007721b2d4
Danilo Kühn <dk@nogo-software.de>
2011-12-14 16:24:38 +0100

    fix small bug in deleting service
    there was no return

--- src/Dime/TimetrackerBundle/Controller/ServicesController.php
+++ src/Dime/TimetrackerBundle/Controller/ServicesController.php
@@ -106,10 +106,9 @@ class ServicesController extends DimeController
      */
     public function deleteServicesAction($id)
     {
-        $em = $this->getDoctrine()->getEntityManager();
-        $service = $em->getRepository('DimeTimetrackerBundle:Service')->find($id);
-        
+        $service = $this->getServiceRepository()->find($id);
         if ($service) {
+            $em = $this->getDoctrine()->getEntityManager();
             $em->remove($service);
             $em->flush();
             
@@ -118,5 +117,6 @@ class ServicesController extends DimeController
             $view = View::create()->setStatusCode(404);
             $view->setData("Service does not exists.");
         }
+        return $this->get('fos_rest.view_handler')->handle($view);
     }
 }
