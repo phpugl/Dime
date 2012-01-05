a42ab322fb38dc3012c7736f91757f494622e5a7
Danilo Kühn <dk@nogo-software.de>
2012-01-05 13:04:48 +0100

    add js activity module

--- src/Dime/TimetrackerFrontendBundle/Resources/public/js/modules/dime.services.js
+++ src/Dime/TimetrackerFrontendBundle/Resources/public/js/modules/dime.services.js
@@ -121,5 +121,46 @@
         this.el.data('modal').hide();
     }
   });
+  
+  service.views.options = Backbone.View.extend({
+    tagName: "option",
+    initialize: function() {
+        _.bindAll(this, 'render');
+    },
+    render: function() {
+        $(this.el).attr('value', this.model.get('id')).html(this.model.get('name'));
+        return this;
+    }
+  });
+
+  service.views.select = Backbone.View.extend({
+    initialize: function(opt){
+        _.bindAll(this, 'addOne', 'addAll');
+        this.collection.bind('reset', this.addAll);
+
+        // grep selected option can be service object or just the id
+        if (opt && opt.selected) {
+          this.selectedId = (opt.selected.id) ? opt.selected.id : opt.selected;
+        }
+    },
+    addOne: function(obj){
+        var optionView = new service.views.options({ model: obj });
+        this.selectViews.push(optionView);
+        $(this.el).append(optionView.render().el);
+    },
+    addAll: function() {
+        // clear select
+        $(this.el).html('');
+
+        _.each(this.selectViews, function(optionView) { optionView.remove(); });
+        this.selectViews = [];
+        this.collection.each(this.addOne);
+
+        // select option if selectedId exists
+        if (this.selectedId) {
+            $(this.el).val(this.selectedId);
+        }
+    }
+  });
 
 })(jQuery, dime);
