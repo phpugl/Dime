e226f9764b76f900e495e5b17577a6aaf41962dc
Sebastian Schulze <github.com@bascht.com>
2012-02-04 16:04:44 +0100

    - set proper precision for Activities and Services

--- src/Dime/TimetrackerBundle/Entity/Activity.php
+++ src/Dime/TimetrackerBundle/Entity/Activity.php
@@ -85,7 +85,7 @@ class Activity {
     /**
      * @var float $rate
      *
-     * @ORM\Column(type="decimal", nullable=true)
+     * @ORM\Column(type="decimal", scale=1, precision=2, nullable=true)
      */
     protected $rate;
 
