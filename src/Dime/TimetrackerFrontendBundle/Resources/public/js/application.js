5556687d57be23cf37906529a65fdf9cae4db251
Danilo Kuehn <dk@nogo-software.de>
2012-02-07 12:56:15 +0100

    rename js namespace from dime to Dime

--- src/Dime/TimetrackerFrontendBundle/Resources/public/js/application.js
+++ src/Dime/TimetrackerFrontendBundle/Resources/public/js/application.js
@@ -4,32 +4,16 @@
 (function ($) {
 
   // Dime namespace
-  window.dime = {
+  window.Dime = {
     collection: {},
     model: {},
     views: {}
   };
 
   $(document).ready(function() {
-    $('.tabs').tabs();
+    //$('.tabs').tab('show');
 
-    $('nav').dropdown();
-
-    $('table#dataTable').button();
-
-    $('.alert-message').alert();
-
-    $('.client-project').twipsy({
-      live: true,
-      delayIn: 3000
-    });
-
-    $('#magic-data-entry').popover({
-      offset: 10,
-      placement: 'below',
-      delayIn: 3000,
-      trigger: focus
-    });
+    //$('nav').dropdown();
 
     //$('.loading').css({height: '50px', width: '100%'}).spin('large', 'black');
   });
