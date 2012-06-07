2a5861ab7835bd52c6a277f8f055cb53ecdbec09
Frank Weigmann <sunrise2k4@googlemail.com>
2012-06-07 12:14:45 +0200

    add .idea to .gitignore change ip that can connect to the webapp

--- web/app_dev.php
+++ web/app_dev.php
@@ -11,7 +11,7 @@ if (isset($_SERVER['HTTP_CLIENT_IP'])
     || !in_array(@$_SERVER['REMOTE_ADDR'], array(
         '127.0.0.1',
         '::1',
-        '33.33.33.1'
+        '192.168.33.1'
     ))
 ) {
     header('HTTP/1.0 403 Forbidden');
