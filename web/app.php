6f2782332c7a13d430f74689f3739483160a5fb6
Danilo Kuehn <dk@nogo-software.de>
2014-06-18 13:07:09 +0200

    update to Symfony 2.4

--- web/app.php
+++ web/app.php
@@ -8,8 +8,9 @@ $loader = require_once __DIR__.'/../app/bootstrap.php.cache';
 // Use APC for autoloading to improve performance.
 // Change 'sf2' to a unique prefix in order to prevent cache key conflicts
 // with other applications also using APC.
-$loader = new ApcClassLoader('DimeTimetracker', $loader);
-$loader->register(true);
+$apcLoader = new ApcClassLoader('DimeTimetracker', $loader);
+$loader->unregister();
+$apcLoader->register(true);
 
 require_once __DIR__.'/../app/AppKernel.php';
 //require_once __DIR__.'/../app/AppCache.php';
@@ -17,6 +18,9 @@ require_once __DIR__.'/../app/AppKernel.php';
 $kernel = new AppKernel('prod', false);
 $kernel->loadClassCache();
 //$kernel = new AppCache($kernel);
+
+// When using the HttpCache, you need to call the method in your front controller instead of relying on the configuration parameter
+//Request::enableHttpMethodParameterOverride();
 $request = Request::createFromGlobals();
 $response = $kernel->handle($request);
 $response->send();
